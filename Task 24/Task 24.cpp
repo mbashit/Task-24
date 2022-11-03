9cdfb4c5ead028f320876b2f1e1c50d2baed25bb
Bashitapwa Michael <michaelbashitapwa52@gmail.com>
2022-11-04 01:31:47 +0300

    yoh

--- Task 24/Task 24.cpp
+++ Task 24/Task 24.cpp
@@ -8,38 +8,95 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void enter_array_values(int* array, int size);
+int sum_of_digits(int num);
+int max_sum_of_digits(int* array, int size);
 
-    int* a = NULL;
-    int* b = NULL;
-    int* c = NULL;
+int main()
+{
+    int* A = nullptr;
+    int* B = nullptr;
+    int* C = nullptr;
 
-    int  n;
-    cout << "enter the number:";
+    int n;
+    cout << "enter the size of array: ";
     cin >> n;
 
-    a = new int[n];
-    b = new int[n];
-    c = new int[n];
+    A = new int[n];
+    B = new int[n];
+    C = new int[2 * n];
 
-    cout << "Enter elements into array A\n";
-    for (int index = 0; index < n; index++) 
-        cin >> a[index];
+    cout << "For array A:" << endl;
+    enter_array_values(A, n);
+    cout << "For array B:" << endl;
+    enter_array_values(B, n);
+
+    int max_sum_A = max_sum_of_digits(A, n);
+    int max_sum_B = max_sum_of_digits(B, n);
+
+    if (max_sum_A > max_sum_B)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            C[i] = A[i];
+        }
+        for (int i = n; i < 2 * n; i++)
+        {
+            C[i] = B[i - n];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            C[i] = B[i];
+        }
+        for (int i = n; i < 2 * n; i++)
+        {
+            C[i] = A[i - n];
+        }
+    }
+
+    for (int i = 0; i < 2 * n; i++)
+    {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+}
+
+void enter_array_values(int* array, int size)
+{
+    for (int i = 0; i <= size - 1; i++)
+    {
+        cout << "Enter element number " << i + 1 << ": ";
+        cin >> array[i];
+    }
+}
+
+int sum_of_digits(int num)
+{
+    int digit, sum = 0;
+    while (num != 0)
+    {
+        digit = num % 10;
+        sum += digit;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+int max_sum_of_digits(int* array, int size)
+{
+    int max = sum_of_digits(array[0]);
+    for (int i = 1; i <= size - 1; i++)
+    {
+        if (max < sum_of_digits(array[i]))
+        {
+            max = sum_of_digits(array[i]);
+        }
+    }
+
+    return max;
+}
     
-    cout << "Enter elelments into array B\n";
-    for (int index = 0; index < n; index++) 
-        cin >> b[index];
-    
-
-
-    for (int index = 0; index < n; index++) 
-        c[index] = a[index] + b[index];
-
-        cout << "Array A\t\t Array B \t\t  Array C\n" << endl;
-    for (int index = 0; index < 0; index++) 
-        cout << "\t" << a[index] << "t\t\t" << b[index] << "t\t\t" << c[index] << endl;
-    
-         return 0;
-         }
-
-
